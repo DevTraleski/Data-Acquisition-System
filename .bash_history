fab59d206d05a224e4580e0ec06f879ab3e94044
DevTraleski <dev.traleski@gmail.com>
2019-02-12 21:08:45 +0000

    Managed to send multicast, and encrypt everythin

--- .bash_history
+++ .bash_history
@@ -101,3 +101,93 @@ cd
 ls
 nano Networker.py 
 exit
+cd
+git add --all .
+git commit -m "Implemented coap client sending to multicast"
+git config --global user.email "dev.traleski@gmail.com"
+git config --global user.name "DevTraleski"
+git commit -m "Implemented coap client sending to multicast"
+ls
+git push
+exit
+cd
+ls
+nano main.py 
+nano Networker.py 
+exit
+cd
+ls
+nano Networker.py 
+exti
+drs gw
+exit
+cd
+ls
+nano Networker.py 
+exit
+cd
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+exit
+cd
+ls
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+exit
+cd
+ls
+cat Networker.py 
+less Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+nano Networker.py 
+python3 Networker.py 
+python3 Networker.py 
+nano Networker.py 
+nano db
+nano Networker.py 
+exit
--- Networker.py
+++ Networker.py
@@ -2,6 +2,9 @@ import http.client
 import json
 
 from coapthon.client.helperclient import HelperClient
+from coapthon import defines
+from coapthon.messages.message import Message
+from coapthon.messages.request import Request
 
 from Crypto.Cipher import AES
 import hashlib
@@ -47,30 +50,50 @@ class Networker:
 		now = time.time()
 		timeCode = int(datetime.datetime.fromtimestamp(now).strftime('%Y%m%d%H%M%S'))
 		timeStamp = datetime.datetime.fromtimestamp(now).strftime('%Y-%m-%d %H:%M:%S')
+		print("Time Code: " + str(timeCode))
 
 		groupKey = hotp.at(timeCode)
 		m = hashlib.md5()
 		m.update(groupKey.encode("UTF-8"))
-		hashedKey = m.hexdigest()
+		hashedKey = m.hexdigest()[:16]
+		print("Group Key: " + str(groupKey))
+		print("Hashed Key: " + str(hashedKey))
 
 		IV = os.urandom(16)
+		print("Original IV: " + str(IV))
 		encryptor = AES.new(hashedKey, AES.MODE_CBC, IV=IV)
 		length = 16 - (len(request) % 16)
 		data = bytes([length])*length
 		request += data.decode("utf-8")
+		print("Request with padding: " + request)
 		cipherText = encryptor.encrypt(request)
-		return self.sendReq(binascii.hexlify(cipherText).upper())
+		print("Original Data: " + str(cipherText))
+		return self.send(binascii.hexlify(cipherText).upper(), timeStamp, binascii.hexlify(IV).upper())
 
-	def sendReq(self, req):
-		client = HelperClient(server=("224.0.0.0", 5683))
-		response = client.get("basic/")
-		print("Response: " + response.pretty_print())
+	def send(self, data, timestamp, iv):
+		print("Hexed Data: " + str(data))
+		print("Hexed IV: " + str(iv))
+		client = HelperClient(server=("224.0.1.187", 5001))
+		
+		dict = { "data" : str(data)[2:-1], "timestamp": timestamp, "iv": str(iv)[2:-1] }
+		jsonStr = json.dumps(dict)
+		print(jsonStr)	
+	
+		request = Request()
+		request.destination = client.server
+		request.code = defines.Codes.GET.number
+		request.uri_path = 'info/'
+		request.payload = jsonStr
+
+		response = client.send_request(request)
 		client.stop()
-		return response.pretty_print()
+
+		print("Response: " + response.pretty_print())
+		return response.payload
 		
 		
 
-#print(Networker().req("Searching"))
+print(Networker().req("infox"))
 
 #decipher = AES.new(hashedKey, AES.MODE_CBC, IV)
 #plainText = decipher.decrypt(cipherText)
